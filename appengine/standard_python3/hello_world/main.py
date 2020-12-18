f9eefb90e3d4db4eee435beb52c2eaa55dec0e63
akila13-28 <55209152+akila13-28@users.noreply.github.com>
2020-12-18 15:38:59 +0530

    Update main.py

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Features of Java 1.Platform Independent 2.Simple 3.Secure 4.Architecture-neutral'
 
 
 if __name__ == '__main__':
